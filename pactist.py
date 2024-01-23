4062aeab6d6944a6d4174c6bb312fe485e4b2c12
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-23 10:07:32 +0200

    Add files via upload

---
+++ pactist.py
@@ -0,0 +1,8 @@
+number = 1000
+
+print("the number pi is {:.3f}".format(number))
+print("the  number is {:,}.".format(number))
+print("the  numbe is {:b}".format(number))
+print("the number is {:o}".format(number))
+print("the number is {:X}".format(number))
+print("the number is {:E}".format(number))
